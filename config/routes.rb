a7d0f5b5fd08615483877b60ec3612ec19e75637
Larry Meng <dexinmeng@qq.com>
2013-10-30 23:20:02 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
 
+  resources :users
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
